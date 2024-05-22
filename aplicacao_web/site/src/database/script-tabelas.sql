779bdbbd162095bdd77e4f60ca8331cbd6821507
MaikonDsGomes <maikon.gomes@sptech.school>
2024-05-21 22:09:23 -0300

    UP: MSQL banco de dados

--- aplicacao_web/site/src/database/script-tabelas.sql
+++ aplicacao_web/site/src/database/script-tabelas.sql
@@ -8,8 +8,10 @@ comandos para mysql server
 
 create database projetoIndividual;
 
+
 use projetoIndividual;
 
+-- drop database projetoindividual;
 
 create table atleta (
 	idAtleta int primary key auto_increment,
@@ -27,7 +29,6 @@ create table atleta (
 	grau char(2),
     descricao varchar(500)
 );
-    
 
 create table medalha (
 	idMedalha int primary key auto_increment,
@@ -39,7 +40,6 @@ create table medalha (
     constraint medalhaAtleta foreign key (fkAtleta) references atleta(idAtleta)
 );
 
-
 create table patrocinio (
 	idPatrocinio int primary key auto_increment,
     lugar varchar(45),
@@ -49,39 +49,63 @@ create table patrocinio (
     constraint patrocinioAtleta foreign key (fkAtleta) references atleta(idAtleta)
 );
 
--- INSERTS
+select * from atleta;
 
--- Inserts para a tabela 'atleta'
+create table postagem(
+	idPostagem int primary key auto_increment,
+    titulo varchar(45),
+    descricao varchar(500),
+    dtPostagem date,
+    horaPostagem time,
+    fkAtleta int,
+    constraint postageFkAtleta foreign key (fkAtleta) references atleta(idAtleta)
+);
+
+-- SELECT a.id AS idAviso, a.titulo, a.descricao, a.fk_usuario, u.id AS idUsuario, u.nome, u.email, u.senha
+-- FROM postagem a INNER JOIN usuario u ON a.fk_usuario = u.id WHERE u.id = 6
+
+select idPostagem, titulo, postagem.descricao, postagem.fkAtleta, dtPostagem, horaPostagem,idAtleta, nome, email, senha
+from postagem join atleta on postagem.fkAtleta = idAtleta;
+
 insert into atleta (nome, sobrenome, email, senha, telefone, cep, logradouro, complemento, dtNasc, team, faixa, grau, descricao) values
     ('João', 'Silva', 'joao.silva@email.com', '123456789', '(00) 1234-5678', '12345-678', 'Rua horizonte', 'Apartamento 101', '1990-05-15', 'Macaco gold team', 'preta', '1°', 'Campeão regional de Jiu-Jitsu'),
     ('Maria', 'Santos', 'maria.santos@email.com', '123456789', '(00) 9876-5432', '54321-876', 'Avenida paulista', 'apartamento 1220', '1988-10-20', 'Alpha', 'azul', '2°', 'Participante em torneios estaduais de Jiu-Jitsu'),
     ('Pedro', 'Souza', 'pedro.souza@email.com', '123456789', '(00) 4567-8901', '98765-432', 'Rua Cristovão', 'Bloco D', '1995-03-10', 'Ryan gracie', 'marrom', '3°', 'Instrutor de Jiu-Jitsu');
 
--- Inserts para a tabela 'medalha'
-insert into medalha (federacao, campeonato, ano, posicao, fkAtleta) values
-    ('CBJJE', 'Campeonato Brasileiro', '2023-01-01', '1º', 1),
-    ('IBJJF', 'Mundial de Jiu-Jitsu', '2022-01-01', '3º', 2),
-    ('FPJJ', 'Copa Paulista', '2024-01-01', '2º', 3);
 
--- Inserts para a tabela 'patrocinio'
-insert into patrocinio (lugar, empresa, segmento, fkAtleta) values
-    ('pernas', 'Kvara', 'Vestes', 1),
-    ('peito', 'Growth', 'Alimentício', 2),
-    ('braços', 'cisco', 'Tech', 3);
-    
+INSERT INTO patrocinio (lugar, empresa, segmento, fkAtleta) VALUES ('Costas', 'Empresa1', 'Segmento1', 1);
+INSERT INTO patrocinio (lugar, empresa, segmento, fkAtleta) VALUES ('Costas', 'Empresa2', 'Segmento2', 1);
 
--- select para exibir as medalhas e os patrocinios de um usuario especifico
-select a.nome, a.sobrenome, m.federacao, m.campeonato, m.ano, m.posicao, pt.empresa, pt.segmento
-from atleta a
-left join medalha m on a.idAtleta = m.fkAtleta
-left join patrocinio pt on a.idAtleta = pt.fkAtleta
-where a.idAtleta = 1; -- substitua "1" pelo ID do usuário desejado
+INSERT INTO patrocinio (lugar, empresa, segmento, fkAtleta) VALUES ('Costas', 'Empresa3', 'Segmento3', 2);
+INSERT INTO patrocinio (lugar, empresa, segmento, fkAtleta) VALUES ('Costas', 'Empresa4', 'Segmento4', 2);
 
--- select para exibir todas as medalhas e patrocinios de todos os usuarios
-select a.nome, a.sobrenome, m.federacao, m.campeonato, m.ano, m.posicao, pt.empresa, pt.segmento
-from atleta a
-left join medalha m on a.idAtleta = m.fkAtleta
-left join patrocinio pt on a.idAtleta = pt.fkAtleta;
+INSERT INTO patrocinio (lugar, empresa, segmento, fkAtleta) VALUES ('Costas', 'Empresa5', 'Segmento5', 3);
+INSERT INTO patrocinio (lugar, empresa, segmento, fkAtleta) VALUES ('Costas', 'Empresa6', 'Segmento6', 3);
+
+
+select idPatrocinio, lugar, empresa, segmento, patrocinio.fkAtleta, nome, email, senha from patrocinio
+join atleta on patrocinio.fkAtleta;
+
+INSERT INTO postagem (idPostagem, titulo, descricao, dtPostagem, horaPostagem, fkAtleta) 
+VALUES (1, 'Primeiro Post', 'Descrição do primeiro post.', '2024-05-21', '10:00:00', 1);
+
+INSERT INTO postagem (idPostagem, titulo, descricao, dtPostagem, horaPostagem, fkAtleta) 
+VALUES (2, 'Segundo Post', 'Descrição do segundo post.', '2024-05-22', '11:00:00', 2);
+
+INSERT INTO postagem (idPostagem, titulo, descricao, dtPostagem, horaPostagem, fkAtleta) 
+VALUES (3, 'Terceiro Post', 'Descrição do terceiro post.', '2024-05-23', '12:00:00', 3);
+
+
+INSERT INTO medalha (federacao, campeonato, ano, posicao, fkAtleta) 
+VALUES ('Federação A', 'Campeonato X', '2022-05-20', '1', 1);
+
+INSERT INTO medalha (federacao, campeonato, ano, posicao, fkAtleta) 
+VALUES ('Federação B', 'Campeonato Y', '2023-06-15', '2', 2);
+
+INSERT INTO medalha (federacao, campeonato, ano, posicao, fkAtleta) 
+VALUES ('Federação C', 'Campeonato Z', '2021-07-10', '3', 3);
+
+select federacao, campeonato, ano, posicao, fkAtleta from medalha where fkAtleta = 1;
 
 
 
