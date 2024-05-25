42e785abbbbbc8910dfaf5bc7439d0ecac87a1d6
MaikonDsGomes <maikon.gomes@sptech.school>
2024-05-25 13:30:15 -0300

    add: grafico integrado atleta p/ faixa FEITO!

--- aplicacao_web/site/src/models/medidaModel.js
+++ aplicacao_web/site/src/models/medidaModel.js
@@ -1,29 +1,35 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idAquario, limite_linhas) {
+function buscarUltimasMedidas() {
 
-    var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    FROM medida
-                    WHERE fk_aquario = ${idAquario}
-                    ORDER BY id DESC LIMIT ${limite_linhas}`;
+    var instrucaoSql = `select 'branca' as faixa, count(*) as quantidade from atleta where faixa = 'branca'
+    union all
+    select 'azul' as faixa, count(*) as quantidade from atleta where faixa = 'azul'
+    union all
+    select 'roxa' as faixa, count(*) as quantidade from atleta where faixa = 'roxa'
+    union all
+    select 'marrom' as faixa, count(*) as quantidade from atleta where faixa = 'marrom'
+    union all
+    select 'preta' as faixa, count(*) as quantidade from atleta where faixa = 'preta';
+    `;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function buscarMedidasEmTempoReal(idAquario) {
+function buscarMedidasEmTempoReal() {
 
-    var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        FROM medida WHERE fk_aquario = ${idAquario} 
-                    ORDER BY id DESC LIMIT 1`;
+    var instrucaoSql = `
+    select 'branca' as faixa, count(*) as quantidade from atleta where faixa = 'branca'
+union all
+select 'azul' as faixa, count(*) as quantidade from atleta where faixa = 'azul'
+union all
+select 'roxa' as faixa, count(*) as quantidade from atleta where faixa = 'roxa'
+union all
+select 'marrom' as faixa, count(*) as quantidade from atleta where faixa = 'marrom'
+union all
+select 'preta' as faixa, count(*) as quantidade from atleta where faixa = 'preta';
+                    `;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
